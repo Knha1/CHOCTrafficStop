1125a5b68ac3bf3bbf5859b8660db6a0407efb74
Minhduc Cao <minhduc.t.cao@gmail.com>
2021-04-05 22:31:43 -0700

    Fixed minor typo in SettingsScreen import

--- app/screens/SettingsScreen.js
+++ app/screens/SettingsScreen.js
@@ -1,33 +1,44 @@
-import React from "React";
+import React from "react";
 import { Text, View, StyleSheet, Button, Modal } from "react-native";
-import {widthPercentageToDP as wp, heightPercentageToDP as hp} from 'react-native-responsive-screen';
+import {
+	widthPercentageToDP as wp,
+	heightPercentageToDP as hp,
+} from "react-native-responsive-screen";
 import colors from "../config/colors";
 
-function SettingsScreen({navigation}) {
-    return (
-        
-        <View styles={styles.container}>
-
-            {/*container for the text*/}
-            <View styles={styles.container}>
-                <Text style={{alignSelf:'center', fontSize:hp('3%'), paddingTop: 20, paddingBottom: 10}}>Name: Jonathan Doe</Text>
-                <Text style={{alignSelf:'center', fontSize:hp('3%'), paddingBottom: 20}}>Language: English</Text>
-            </View>
-
-            {/*Cointainer for the buttons*/}            
-            <View styles={styles.cointainer}>
-
-            <View style={styles.buttonSpacing}>
-				<Button title="Change Name" />
+function SettingsScreen({ navigation }) {
+	return (
+		<View styles={styles.container}>
+			{/*container for the text*/}
+			<View styles={styles.container}>
+				<Text
+					style={{
+						alignSelf: "center",
+						fontSize: hp("3%"),
+						paddingTop: 20,
+						paddingBottom: 10,
+					}}
+				>
+					Name: Jonathan Doe
+				</Text>
+				<Text
+					style={{ alignSelf: "center", fontSize: hp("3%"), paddingBottom: 20 }}
+				>
+					Language: English
+				</Text>
 			</View>
-             <View style={styles.buttonSpacing}>
-				<Button title="Change Language" />
-			</View>
 
-            </View>
-            
-        </View>
-    );
+			{/*Cointainer for the buttons*/}
+			<View styles={styles.cointainer}>
+				<View style={styles.buttonSpacing}>
+					<Button title="Change Name" />
+				</View>
+				<View style={styles.buttonSpacing}>
+					<Button title="Change Language" />
+				</View>
+			</View>
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
@@ -40,11 +51,10 @@ const styles = StyleSheet.create({
 
 	buttonSpacing: {
 		margin: 5,
-        width: 250,
-        alignSelf: "center",
-        padding: 4,
+		width: 250,
+		alignSelf: "center",
+		padding: 4,
 	},
-
 });
 
 export default SettingsScreen;
