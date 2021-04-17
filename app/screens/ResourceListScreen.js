8a736ce485d4049cb7af9eade3d008a11910bbc3
angiemrtin <angelaim@uci.edu>
2021-04-16 20:53:04 -0700

    Update ResourceListScreen.js

--- app/screens/ResourceListScreen.js
+++ app/screens/ResourceListScreen.js
@@ -29,7 +29,7 @@ function ResourceListScreen({ navigation }) {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: colors.bg,
+		backgroundColor: '#F9F9F9',
 		paddingTop: 20,
 	},
 	sectionHeader: {
