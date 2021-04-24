68c0820b59bc70d610dc65c6176ef23ffd784711
Minhduc Cao <minhduc.t.cao@gmail.com>
2021-04-23 23:56:47 -0700

    Completed resources and questions storage from DB

--- App.js
+++ App.js
@@ -19,10 +19,74 @@ import { Settings } from "react-native";
 import ResourceRecommendedScreen from "./app/screens/ResourceRecommendedScreen";
 import SurveyCategoriesScreen from "./app/screens/SurveyCategoriesScreen";
 import YesNoQuestionScreen from "./app/screens/YesNoQuestionScreen";
+import { firebase } from "./app/firebase/config";
 
 const Stack = createStackNavigator();
 
 export default function App() {
+	// Get resources from DB, declare as global
+	global.resources = null;
+	firebase
+		.database()
+		.ref()
+		.child("resource")
+		.get()
+		.then((snapshot) => {
+			if (snapshot.exists()) {
+				var tempResources = [];
+
+				snapshot.forEach((child) => {
+					// Ignore the num_resources variable, store the rest of the resource
+					if (!Number.isInteger(child.val())) {
+						tempResources.push({
+							resource_id: child.val().resource_id,
+							name: child.val().address,
+							description: child.val().description,
+							category: child.val().category,
+							organization: child.val().organization,
+							address: child.val().address,
+							phone_num: child.val().phone_num,
+							availability: child.val().availability,
+							website: child.val().website,
+						});
+					}
+				});
+
+				global.resources = tempResources;
+			} else {
+				console.log("'resource' data retrieval from DB was unsuccessful.");
+			}
+		});
+
+	// Get questions from DB, declare as global
+	global.questions = null;
+	firebase
+		.database()
+		.ref()
+		.child("question")
+		.get()
+		.then((snapshot) => {
+			if (snapshot.exists()) {
+				var tempQuestions = [];
+
+				// Ignore the num_questions variable, store the rest of the resource
+				if (!Number.isInteger(child.val())) {
+					tempQuestions.push({
+						category: child.val().category,
+						order: child.val().order,
+						question_id: child.val().question_id,
+						text: child.val().text,
+						type: child.val().type,
+						answer_choices: child.val().answer_choices,
+					});
+				}
+
+				global.questions = tempQuestions;
+			} else {
+				console.log("'question' data retrieval from DB was unsuccessful.");
+			}
+		});
+
 	return (
 		<NavigationContainer>
 			<Stack.Navigator>
