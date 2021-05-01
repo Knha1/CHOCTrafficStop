7cbad3327d9ea3dc5d26effc2a98441e67ee42dd
Knha1 <60526919+Knha1@users.noreply.github.com>
2021-05-01 15:39:32 -0700

    AdminHomeScreen & ExportData BaseUI

--- app/screens/admin/AdminHomeScreen.js
+++ app/screens/admin/AdminHomeScreen.js
@@ -1,49 +1,78 @@
 import React, { useState } from "react";
-import { Text, View, StyleSheet, Button, Modal } from "react-native";
+import { Text, View, StyleSheet, Button, Modal, ImageBackground, TouchableOpacity, Image } from "react-native";
 
 import colors from "../../config/colors";
+import schedule from "../../assets/schedule1.png";
+import clipboard from "../../assets/clipboard1.png";
+import file from "../../assets/file.png";
+import bg from "../../assets/background.png";
+
 
 function AdminHomeScreen({ navigation }) {
 	const [modalVisible, setModalVisible] = useState(false);
 	return (
-		<View style={styles.container}>
-			<View style={styles.container}>
-				<View style={styles.buttonSpacing}>
-					<Button title="View Statistics"
-							onPress={() => navigation.navigate("Statistics Home")}
-					/>
-				</View>
-				<View style={styles.buttonSpacing}>
-					<Button
-						title="Edit/View Resource List"
-						onPress={() => navigation.navigate("Statistics Details")}
-					/>
-				</View>
-				<View style={styles.buttonSpacing}>
-					<Button
-						title="Export Data"
-						onPress={() => navigation.navigate("Export Data")}
-					/>
+		<View style={[styles.container]}>
+			<ImageBackground
+				source={bg}
+				style={{
+					overflow: "hidden",
+					resizeMode: "stretch",
+					height: "100%",
+					width: "100%"
+				}}
+			>
+				<View style={[styles.base ,{height: "70%", padding: 20, alignItems: "center" }]}>
+					<TouchableOpacity
+						onPress={() => navigation.navigate("Statistics Home")}
+						style={styles.card}
+					>
+						<Text style={styles.buttonText}>View Statistics</Text>
+						<Image source={clipboard} style={styles.icon} />
+					</TouchableOpacity>
 
-				<View style={styles.buttonSpacing}>
-					<Button
-						title="Add Resource +"
-						color = "red"
-						onPress={() =>
-							navigation.navigate('Add Resource')
-						}
-						/>
-					</View>
+					<TouchableOpacity
+						onPress={() => navigation.navigate("Statistics Details")}
+						style={styles.card}
+					>
+						<Text style={styles.buttonText}>Edit/View Resource List</Text>
+						<Image source={schedule} style={styles.icon} />
+					</TouchableOpacity>
+
+					<TouchableOpacity
+						onPress={() => navigation.navigate("Export Data")}
+						style={styles.card}
+					>
+						<Text style={styles.buttonText}>Export Data</Text>
+						<Image source={file} style={styles.icon} />
+					</TouchableOpacity>
+
+					<TouchableOpacity
+						onPress={() => setModalVisible(true)}
+						style={{
+							alignSelf: "center",
+							alignItems: "center",
+							backgroundColor: "#4B9E76",
+							width: "80%",
+							padding: 10,
+							borderRadius: 20,
+							shadowColor: "#000",
+							shadowOffset: { width: 0, height: 2 },
+							shadowOpacity: 0.25,
+							shadowRadius: 4,
+							}}
+					>
+						<Text style={{ color: "white" }}>Add Resource +</Text>
+					</TouchableOpacity>
 				</View>
-			</View>
+			</ImageBackground>
 		</View>
+
 	);
 }
 
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: colors.bg,
 		alignItems: "center",
 		justifyContent: "center",
 	},
@@ -72,6 +101,40 @@ const styles = StyleSheet.create({
 	},
 	emergencyChoice: {
 		flexDirection: "row",
+	},
+	base: {
+		marginTop: "20%",
+		backgroundColor: '#F1F2F2',
+		borderTopRightRadius: 30,
+		borderTopLeftRadius: 30,
+		alignSelf: "stretch",
+		flex: 1
+	},
+	card: {
+		backgroundColor: "white",
+		width: "80%",
+		height: "10%",
+		borderRadius: 20,
+		marginBottom: 20,
+		alignItems: "center",
+		justifyContent: "space-between",
+		flexDirection: "row",
+		shadowColor: "#000",
+		shadowOffset: { width: 0, height: 2 },
+		shadowOpacity: 0.25,
+		shadowRadius: 4,
+	},
+	buttonText: {
+		textAlign: "left",
+		marginLeft: "10%",
+		color: "#003C98",
+		flexWrap: "wrap",
+		width: "50%"
+	},
+	icon: {
+		resizeMode: "contain",
+		width: "40%",
+		height: "50%",
 	},
 });
 
--- app/screens/admin/ExportDataScreen.js
+++ app/screens/admin/ExportDataScreen.js
@@ -1,47 +1,108 @@
 import React, { useState } from "react";
-import { Text, View, StyleSheet, Button, Modal, TextInput } from "react-native";
+import { Text, View, StyleSheet, Button, Modal, TextInput, ImageBackground } from "react-native";
 import DropDownPicker from 'react-native-dropdown-picker';
 
 import colors from "../../config/colors";
+import bg from "../../assets/background.png";
+import { TouchableOpacity } from "react-native-gesture-handler";
 
 function ExportDataScreen({ navigation }) {
 	return (
-        <View style={{padding: 10}}>
+		<View style={styles.container}>
+			<ImageBackground source={bg} style={{overflow: "hidden",resizeMode: "stretch",height: "100%", width: "100%"}}>
+				<View style={styles.base}>
+					<Text style={{
+							marginTop: "10%",
+							fontSize: 26,
+							alignSelf: "flex-start",
+							textAlign: "left",
+							marginLeft: "10%",
+							marginBottom: "5%",
+							color: "#003C98",
+							fontWeight: 'bold'
 
-        <Text>File Name: </Text>
-        <TextInput 
-        style={{height: 40}}
-        placeholder = "File Name" />
-		<View style ={{marginTop: 10, margin:5}}/>
-		<Text>File Type: </Text>
-		<DropDownPicker
-			items={[
-				{label: '.pdf', value: 'pdf'},
-				{label: '.docx', value: 'docx'},
-				{label: '.txt', value: 'txt'},
-				{label: '.odt', value: 'odt'},
-			]}
-		/>
+						}}>
+							Export Data 
+					</Text>
+					<Text style={styles.subheader}>File Name: </Text>
+					<TextInput style={styles.baseText} placeholder = " File Name" />
+					<Text style={styles.subheader}>File Format: </Text>
+					<DropDownPicker
+						containerStyle={{marginHorizontal:"10%", height: "5%"}}
+						itemStyle={{marginHorizontal: "10%", justifyContent: "flex-start"}}
+						items={[
+							{label: '.pdf', value: 'pdf'},
+							{label: '.docx', value: 'docx'},
+							{label: '.txt', value: 'txt'},
+							{label: '.odt', value: 'odt'},
+						]}
+					/>
 
-        <View style ={{marginTop: 10, margin:5}}>
-        <Button
-          title="Export Data"
-          onPress={() =>
-            navigation.navigate('Admin Home')
-          }
-        />
-        </View>
+					<View style={{flexDirection: "row", position: "absolute", bottom: 30, left: 40 }}>
+						<TouchableOpacity onPress={() =>navigation.navigate('Admin Home')} 
+						style={{
+							backgroundColor: "#DFDFDF",
+							alignSelf: "center",
+							borderRadius: 20,
+							padding: 15,
+							marginHorizontal: 10
+						}}>
+							<Text style={[styles.buttonText, {color: "#003C98"}]}>Cancel</Text>
 
-        <View style ={{marginTop: 10, margin:5}}>
-        <Button
-          title="Cancel"
-		  color = "grey"
-          onPress={() =>
-            navigation.navigate('Admin Home')
-          }
-        />
-        </View>
-       </View>
+						</TouchableOpacity>
+						<TouchableOpacity onPress={() =>navigation.navigate('Admin Home')}
+						style={{
+							backgroundColor: "#0E4B9D",
+							alignSelf: "center",
+							borderRadius: 20,
+							padding: 15,
+							marginHorizontal: 10
+						}}>
+							<Text style={[styles.buttonText, {color: "#FFFFFF"}]}>Export Data</Text>
+						</TouchableOpacity>
+					</View>
+						
+
+
+				</View>
+			</ImageBackground>
+		</View>
+    //     <View style={{padding: 10}}>
+
+    //     <Text>File Name: </Text>
+    //     <TextInput 
+    //     style={{height: 40}}
+    //     placeholder = "File Name" />
+	// 	<View style ={{marginTop: 10, margin:5}}/>
+	// 	<Text>File Type: </Text>
+	// 	<DropDownPicker
+	// 		items={[
+	// 			{label: '.pdf', value: 'pdf'},
+	// 			{label: '.docx', value: 'docx'},
+	// 			{label: '.txt', value: 'txt'},
+	// 			{label: '.odt', value: 'odt'},
+	// 		]}
+	// 	/>
+
+    //     <View style ={{marginTop: 10, margin:5}}>
+    //     <Button
+    //       title="Export Data"
+    //       onPress={() =>
+    //         navigation.navigate('Admin Home')
+    //       }
+    //     />
+    //     </View>
+
+    //     <View style ={{marginTop: 10, margin:5}}>
+    //     <Button
+    //       title="Cancel"
+	// 	  color = "grey"
+    //       onPress={() =>
+    //         navigation.navigate('Admin Home')
+    //       }
+    //     />
+    //     </View>
+    //    </View>
     );
     
 }
@@ -49,7 +110,6 @@ function ExportDataScreen({ navigation }) {
 const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		backgroundColor: colors.bg,
 		alignItems: "center",
 		justifyContent: "center",
 	},
@@ -79,6 +139,33 @@ const styles = StyleSheet.create({
 	emergencyChoice: {
 		flexDirection: "row",
 	},
+	base: {
+		marginTop: "20%",
+		backgroundColor: 'white',
+		borderTopRightRadius: 30,
+		borderTopLeftRadius: 30,
+		alignSelf: "stretch",
+		flex: 1
+	},
+	baseText:{
+		marginLeft: "10%",
+		marginRight: "10%",
+		marginBottom: "5%",
+		height: "5%",
+		borderWidth: 1,
+		borderRadius: 5,
+		borderColor: "#D1D1D1",
+		backgroundColor: "white"
+	},
+	subheader:{
+		marginLeft: "10%",
+		marginBottom: "1%"
+	},
+	buttonText:{
+		fontSize: 16,
+		textAlign: "center",
+		minWidth: "30%"
+	}
 });
 
 export default ExportDataScreen;
