ee8c1236f99002dd753ae7039dbe08437eec88c2
Minhduc Cao <minhduc.t.cao@gmail.com>
2021-05-19 16:49:33 -0700

    Added working radio buttons for questions
    Co-authored-by: kmphan <kmphan@users.noreply.github.com>

--- app/screens/patient/YesNoQuestionScreen.js
+++ app/screens/patient/YesNoQuestionScreen.js
@@ -16,65 +16,26 @@ import {
 	ActivityIndicator,
 } from "react-native";
 import { readData } from "../../utils/DataHandler";
-
-// var questions = [
-// 	{
-// 		question: "1. Do you have a safe place to stay?",
-// 		answers: [{ option: "Yes" }, { option: "No" }],
-// 	},
-// 	{
-// 		question: "2. Do you need safety planning?",
-// 		answers: [{ option: "Yes" }, { option: "No" }],
-// 	},
-// 	{
-// 		question: "3. Likert Scale Question",
-// 		answers: [
-// 			{ option: "(1) Great" },
-// 			{ option: "(2) Good" },
-// 			{ option: "(3) Okay" },
-// 			{ option: "(4) Surviving" },
-// 			{ option: "(5) Crisis" },
-// 		],
-// 	},
-// 	{
-// 		question: "4. Example Question",
-// 		answers: [{ option: "Yes" }, { option: "No" }],
-// 	},
-// 	{
-// 		question: "5. Example Question",
-// 		answers: [{ option: "Yes" }, { option: "No" }],
-// 	},
-// 	{
-// 		question: "6. Example Question",
-// 		answers: [{ option: "Yes" }, { option: "No" }],
-// 	},
-// ];
-
-const Item = ({ item, onPress, backgroundColor, textColor }) => (
-	<Text style={styles.text}>{item.question}</Text>
-);
+import RadioButtonRN from "radio-buttons-react-native";
+import logo from "../../assets/logo_nobg.png";
 
 function YesNoQuestionScreen({ route, navigation }) {
 	const [isLoading, setLoading] = React.useState(true);
 	const [data, setData] = useState([]);
 	var category = route.params;
+	var finalTags = [];
 	const category_name = category["category"];
-
-	const renderItem = ({ questions }) => {
-		return (
-			<Item
-				item={questions.answers}
-				onPress={() => (backgroundColor = "#3248a8")}
-			/>
-		);
-	};
 
 	const footer = () => {
 		return (
 			<TouchableHighlight
 				underlayColor="#A6E1FF"
 				style={styles.submitButton}
-				onPress={() => navigation.navigate("Resource List")}
+				onPress={() =>
+					navigation.navigate("Resource List", {
+						tags: finalTags,
+					})
+				}
 			>
 				<Text style={{ color: "#FFF" }}>SUBMIT SURVEY</Text>
 			</TouchableHighlight>
@@ -84,23 +45,62 @@ function YesNoQuestionScreen({ route, navigation }) {
 	useEffect(() => {
 		readData("questions")
 			.then((questions) => {
+				console.log("loading questions");
 				// Load 'questions' from AsyncStorage
 				var questions = JSON.parse(questions);
 				var sections = [];
-
 				for (var i = 0; i < questions.length; i++) {
-					// console.log(category_name);
-					// console.log(questions[i]);
+					console.log("loading each question");
 					if (questions[i].category == category_name) {
-						sections.push({
-							innerData: [
-								{
-									text: questions[i].text,
-									type: questions[i].type,
-									// Add more to this later on
-								},
-							],
-						});
+						// If yes/no question
+						console.log("Type of question: " + questions[i].type);
+						if (questions[i].type == "Yes/No") {
+							sections.push({
+								text: questions[i].order + ". " + questions[i].text,
+								type: questions[i].type,
+								choices: [
+									// TODO: change tags' value to tags
+									{ label: "Yes", tags: "tags for yes" },
+									{ label: "No", tags: "tags for no" },
+								],
+								order: questions[i].order,
+
+								// Add more to this later on
+							});
+						}
+						// If Likert scale
+						else {
+							sections.push({
+								text: questions[i].text,
+								type: questions[i].type,
+								choices: [
+									// TODO: change tags' value to tags
+									{
+										label: "1 " + questions[i].answer_choices[1],
+										tags: "tags for 1",
+									},
+									{
+										label: "2 " + questions[i].answer_choices[2],
+										tags: "tags for 2",
+									},
+									{
+										label: "3 " + questions[i].answer_choices[3],
+										tags: "tags for 3",
+									},
+									{
+										label: "4 " + questions[i].answer_choices[4],
+										tags: "tags for 4",
+									},
+									{
+										label: "5 " + questions[i].answer_choices[5],
+										tags: "tags for 5",
+									},
+								],
+								order: questions[i].order,
+
+								// Add more to this later on
+							});
+						}
 					}
 					console.log(sections);
 					setData(sections);
@@ -110,51 +110,33 @@ function YesNoQuestionScreen({ route, navigation }) {
 			.finally(() => setLoading(false));
 	}, [isLoading]);
 
-	//
 	return (
-		<View>
-			{isLoading ? (
-				// If still loading
-				<ActivityIndicator size="small" color="#0000ff" />
-			) : (
-				<View style={styles.container}>
-					<Text style={styles.topText}>{category_name} Survey</Text>
-					<View style={styles.rectangle}></View>
-					<Text style={styles.skipToResultsText}>Skip to Results?</Text>
-					<View style={styles.bottomContainer}>
-						{/* <FlatList
-							contentContainerStyle={{ paddingBottom: 100 }}
-							data={sections}
-							keyExtractor={(item, index) => index.toString()}
-							ListFooterComponent={footer}
-							renderItem={({ item }) => {
-								return (
-									<View>
-										<Text style={styles.text}>{item.question}</Text>
-										<FlatList
-											data={item.answers}
-											keyExtractor={(item, index) => index.toString()}
-											renderItem={({ item: answers, index }) => (
-												<View>
-													<TouchableHighlight
-														underlayColor="#A6E1FF"
-														onPress={() => navigation.navigate("Resource List")}
-														style={styles.button}
-													>
-														<Text style={styles.buttonText}>
-															{answers.option}
-														</Text>
-													</TouchableHighlight>
-												</View>
-											)}
-										/>
-									</View>
-								);
-							}}
-						/> */}
-					</View>
-				</View>
-			)}
+		<View style={styles.container}>
+			<Text style={styles.topText}>{category_name} Survey</Text>
+			<View style={styles.rectangle}></View>
+			<Text style={styles.skipToResultsText}>Skip to Results?</Text>
+			<View style={styles.bottomContainer}>
+				<FlatList
+					contentContainerStyle={{ paddingBottom: 100 }}
+					data={data}
+					keyExtractor={(item, index) => index.toString()}
+					ListFooterComponent={footer}
+					renderItem={({ item }) => {
+						return (
+							<View>
+								<Text style={styles.text}>{item.text}</Text>
+								<RadioButtonRN
+									data={item.choices}
+									selectedBtn={(e) => {
+										finalTags[item.order] = e.tags;
+										console.log(e.tags);
+									}}
+								/>
+							</View>
+						);
+					}}
+				/>
+			</View>
 		</View>
 	);
 }
