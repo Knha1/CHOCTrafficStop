9721e873953c8b60944a82d89d47d9944396989a
angiemrtin <angelaim@uci.edu>
2021-05-23 18:30:57 -0700

    fixed Description input

--- app/screens/admin/EditResourceScreen.js
+++ app/screens/admin/EditResourceScreen.js
@@ -8,17 +8,19 @@ import backArrowWhite from "../../assets/backArrowWhite.png";
 import edit from "../../assets/edit.png";
 
 function EditResourceScreen({ navigation }) {
-	//const [text, onChangeText] = React.useState("Getting Enough Sleep");
-
+	
 	return (
-			<View style={styles.container}>
+			<SafeAreaView style={styles.container}>
 				<Image style={styles.backArrow} source={backArrowWhite}></Image>
-				<ScrollView style={styles.bottomContainer}>
-					<KeyboardAvoidingView
+				<View style={styles.bottomContainer} contentContainerStyle={styles.contentContainer}>
+				<KeyboardAvoidingView
 					behavior="height"
-					style={{ flex: 1 }}
+					style={{flex: 1}}
 					enabled={true}>
-						<Text style={styles.text}>Editing Resource</Text>
+						
+				
+					
+						<Text style={styles.text}>Edit Resource</Text>
 
 						<Text style = {styles.text3}>Title</Text>
 						<TextInput
@@ -58,27 +60,28 @@ function EditResourceScreen({ navigation }) {
 						
 						<Text style = {styles.text3}>Description</Text>
 						<TextInput
-							multiline={true}
-							//textAlignVertical="center"
-							style={styles.descriptionInput}
-							defaultValue = "Getting Enough Sleep provides tips for sleeping better at night. Nulla ultrices sed commodo in id arcu iaculis in urna. Est, massa tristique nunc egestas urna commodo fames duis. Aliquam curabitur congue vel lectus ornare risus lectus. Tortor, sed sed dictum sed tellus amet. Dictum massa elementum sagittis iaculis proin."
+							style={styles.input}
+							defaultValue = "Tips for sleeping better at night."
 						/>
 						
-					</KeyboardAvoidingView>
-					
-
-						<TouchableOpacity
+				</KeyboardAvoidingView>
+				
+				
+				
+				<View style={{position:"absolute", bottom: '15%'}}>
+					<TouchableOpacity
 							onPress={() => navigation.navigate("Statistics Details")}
 							style={styles.cancelButton}>
 							<Text style={{color: '#0E4B9D'}}>Cancel</Text>
-						</TouchableOpacity>
-						<TouchableOpacity
+					</TouchableOpacity>
+					<TouchableOpacity
 							onPress={() => navigation.navigate("Statistics Details")}
 							style={styles.saveButton}>
 							<Text style={{color: 'white'}}>Save Changes</Text>
-						</TouchableOpacity>
-				</ScrollView>
-			</View>
+					</TouchableOpacity>
+				</View>
+				</View>
+			</SafeAreaView>
 		);
 	}
 
@@ -88,21 +91,18 @@ const styles = StyleSheet.create({
 		backgroundColor: "#0066BB",
 		alignItems: "center",
 		justifyContent: "flex-start",
-		padding: 0,
 		paddingTop: 100,
+	},
+	contentContainer: {
+		justifyContent: 'flex-end',
+		//height: '90%',
 	},
 	input: {
 		height: 30,
-		width: 300,
+		width: '80%',
 		left: 40,
-		top: 34,
+		top: 36,
 		fontSize: 18
-	},
-	descriptionInput: {
-		width: 330,
-		left: 40,
-		top: 34,
-		fontSize: 16
 	},
 	backArrow: {
 		height: 34,
@@ -111,40 +111,6 @@ const styles = StyleSheet.create({
 		left: 30,
 		bottom: 25,
 	},
-	skipToResultsText: {
-		color: "#CAEDFF",
-		fontSize: 14,
-		position: "absolute",
-		top: 96,
-		paddingBottom: 100,
-	},
-	iconButton: {
-		position: 'absolute',
-		left: '78%',
-		top: '10%',
-		borderColor:'rgba(0,0,0,0.2)',
-		justifyContent:'center',
-		width:55,
-		height:55,
-		backgroundColor:'#d8d8d8',
-		borderRadius:50,
-	},
-	rectangle: {
-		height: 4,
-		width: 320,
-		borderRadius: 10,
-		position: "absolute",
-		top: 80,
-		backgroundColor: "#FFF",
-	},
-	topText: {
-		fontSize: 20,
-		color: "#FFF",
-		bottom: 40,
-		alignSelf: "center",
-		textAlign: "center",
-		alignItems: "center",
-	},
 	text: {
 		color: "#003C98",
 		top: 34,
@@ -152,23 +118,11 @@ const styles = StyleSheet.create({
 		fontWeight: "bold",
 		fontSize: 24,
 	},
-	button: {
-		top: 55,
-		height: 45,
-		margin: 3,
-		width: 280,
-		borderRadius: 64,
-		alignSelf: "center",
-		backgroundColor: "#F8F8F8",
-		alignItems: "center",
-		justifyContent: "center",
-		borderColor: "#D6D6D6",
-		borderWidth: 1,
-	},
 	bottomContainer: {
 		flex: 1,
-		alignSelf: "stretch",
-		backgroundColor: "#fff",
+		height: '100%',
+		alignSelf:"stretch",
+		backgroundColor: "white",
 		borderTopRightRadius: 30,
 		borderTopLeftRadius: 30,
 	},
@@ -191,15 +145,11 @@ const styles = StyleSheet.create({
 		left: 40,
 		fontWeight: 'bold'
 	},
-	description: {
-		fontSize: 14,
-		width: 320,
-		left: 40,
-		top: 75,
-	},
 	cancelButton: {
 		position: 'absolute',
-		top: '85%',
+		margin: 3,
+		//top: '95%',
+		//bottom: 0,
 		left: 40,
 		alignItems: "center",
 		justifyContent: "center",
@@ -208,12 +158,13 @@ const styles = StyleSheet.create({
 		borderRadius: 30,
 		borderWidth: 2,
 		height: 45,
-		width: 129,
+		width: 129
 	},
 	saveButton: {
 		position: 'absolute',
-		top: '85%',
-		left: 200,
+		margin: 3,
+		left: 180,
+		//top: '95%',
 		alignItems: "center",
 		justifyContent: "center",
 		backgroundColor: "#0E4B9D",
