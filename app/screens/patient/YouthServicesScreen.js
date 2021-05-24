375c8bb65f0f6123bef40b101662466f543f99cb
Minhduc Cao <minhduc.t.cao@gmail.com>
2021-05-23 20:29:41 -0700

    Fixed crashing when clicking on URL

--- app/screens/patient/YouthServicesScreen.js
+++ app/screens/patient/YouthServicesScreen.js
@@ -10,7 +10,7 @@ import {
 	Linking,
 	ImageBackground,
 	Image,
-	ScrollView
+	ScrollView,
 } from "react-native";
 
 import colors from "../../config/colors";
@@ -23,32 +23,70 @@ var sections = [
 	{
 		title: "PROJECT CHOICE",
 		innerData: [
-			{ name: "Individual Case Management", 
-			description: "Work together with Project CHOICE to develop safety plans and receive all the support you need.",
-			website: "https://www.orangewood4you.org/sex_trafficking_csec_services/project-choice/" },
-			{ name: "Safety Planning" , description: "Create a safety plan with the team at Project CHOICE.",
-			website: "https://www.orangewood4you.org/sex_trafficking_csec_services/project-choice/"},
-			{ name: "Medical, Mental Health & Substance Use Support", description: "Receive support for mental health, medical health, and substance abuse.",
-			website: "https://www.orangewood4you.org/sex_trafficking_csec_services/project-choice/" },
-			{ name: "Housing Referrals", description: "Be referred to affordable houing resources.",
-			website: "https://www.orangewood4you.org/sex_trafficking_csec_services/project-choice/"},
-			{ name: "Education & Employment Assistance", description: "Get help and support in finding education and employment opportunities.",
-			website: "https://www.orangewood4you.org/sex_trafficking_csec_services/project-choice/"},
-			{ name: "Court Support & Advocacy", description: "Find someone to support and advocate for you in Court." ,
-			website: "https://www.orangewood4you.org/sex_trafficking_csec_services/project-choice/"},
-			{ name: "Linkages to Community Resources", description: "Discover different kinds of community resources in Orange County.",
-			website: "https://www.orangewood4you.org/sex_trafficking_csec_services/project-choice/"},
+			{
+				name: "Individual Case Management",
+				description:
+					"Work together with Project CHOICE to develop safety plans and receive all the support you need.",
+				website:
+					"https://www.orangewood4you.org/sex_trafficking_csec_services/project-choice/",
+			},
+			{
+				name: "Safety Planning",
+				description: "Create a safety plan with the team at Project CHOICE.",
+				website:
+					"https://www.orangewood4you.org/sex_trafficking_csec_services/project-choice/",
+			},
+			{
+				name: "Medical, Mental Health & Substance Use Support",
+				description:
+					"Receive support for mental health, medical health, and substance abuse.",
+				website:
+					"https://www.orangewood4you.org/sex_trafficking_csec_services/project-choice/",
+			},
+			{
+				name: "Housing Referrals",
+				description: "Be referred to affordable houing resources.",
+				website:
+					"https://www.orangewood4you.org/sex_trafficking_csec_services/project-choice/",
+			},
+			{
+				name: "Education & Employment Assistance",
+				description:
+					"Get help and support in finding education and employment opportunities.",
+				website:
+					"https://www.orangewood4you.org/sex_trafficking_csec_services/project-choice/",
+			},
+			{
+				name: "Court Support & Advocacy",
+				description: "Find someone to support and advocate for you in Court.",
+				website:
+					"https://www.orangewood4you.org/sex_trafficking_csec_services/project-choice/",
+			},
+			{
+				name: "Linkages to Community Resources",
+				description:
+					"Discover different kinds of community resources in Orange County.",
+				website:
+					"https://www.orangewood4you.org/sex_trafficking_csec_services/project-choice/",
+			},
 		],
 	},
 	{
 		title: "WAYMAKERS",
 		innerData: [
-			{ name: "Waymakers: Victim assistance Programs", description: "Victim assistance",
-			website: "https://waymakersoc.org/" },
-			{ name: "Victim Services", description: "safety planning, crisis response, victim assistance for all crimes (human trafficking / rape crisis), court advocacy.",
-			website:  "https://waymakersoc.org/"},
+			{
+				name: "Waymakers: Victim assistance Programs",
+				description: "Victim assistance",
+				website: "https://waymakersoc.org/",
+			},
+			{
+				name: "Victim Services",
+				description:
+					"safety planning, crisis response, victim assistance for all crimes (human trafficking / rape crisis), court advocacy.",
+				website: "https://waymakersoc.org/",
+			},
 		],
-	}
+	},
 ];
 
 const Item = ({ item, onPress, backgroundColor, textColor }) => (
@@ -68,7 +106,7 @@ function YouthServicesScreen({ navigation }) {
 	return (
 		<ScrollView>
 			<TouchableOpacity onPress={() => navigation.goBack()}>
-				<Image source = {back} style = {styles.backButton}></Image>
+				<Image source={back} style={styles.backButton}></Image>
 			</TouchableOpacity>
 			<Text style={styles.header}>Youth Support Services</Text>
 			<FlatList
@@ -78,32 +116,36 @@ function YouthServicesScreen({ navigation }) {
 					const color = "black";
 					const backgroundColor = "white";
 					return (
-						<View >
+						<View>
 							<View>
 								<Text style={styles.title}>{item.title}</Text>
 							</View>
 
 							<FlatList
-								contentContainerStyle={{ paddingBottom: 10}}
+								contentContainerStyle={{ paddingBottom: 10 }}
 								data={item.innerData}
 								keyExtractor={(item, index) => index.toString()}
 								renderItem={({ item: innerData, index }) => (
 									<View style={styles.cards}>
 										<ImageBackground
-										source={bg}
-										style={{
-											overflow: "hidden",
-											resizeMode: "stretch",
-											paddingBottom: 10,
-											borderRadius: 15,
-										}}
+											source={bg}
+											style={{
+												overflow: "hidden",
+												resizeMode: "stretch",
+												paddingBottom: 10,
+												borderRadius: 15,
+											}}
 										>
 											<TouchableOpacity
 												style={styles.links}
-												onPress={() => navigation.navigate(Linking.openURL(innerData.website))}
+												onPress={() => Linking.openURL(innerData.website)}
 											>
-												<Text style={styles.resourceTitle}>{innerData.name}</Text>
-												<Text style={styles.descText}>{innerData.description}</Text>
+												<Text style={styles.resourceTitle}>
+													{innerData.name}
+												</Text>
+												<Text style={styles.descText}>
+													{innerData.description}
+												</Text>
 											</TouchableOpacity>
 										</ImageBackground>
 									</View>
@@ -128,9 +170,8 @@ const styles = StyleSheet.create({
 		fontSize: 20,
 		paddingTop: 20,
 		paddingBottom: 12,
-		marginTop: '10%',
+		marginTop: "10%",
 		position: "absolute",
-		
 	},
 	subtext: {
 		fontSize: 14,
@@ -144,7 +185,6 @@ const styles = StyleSheet.create({
 		paddingLeft: 10,
 		// paddingBottom: 20,
 		paddingTop: 10,
-		
 	},
 	cards: {
 		backgroundColor: "white",
@@ -162,8 +202,8 @@ const styles = StyleSheet.create({
 			height: 2,
 		},
 		// borderWidth: 1,
-    	// borderColor: "#56E9CE",
-    // borderRadius: 50,
+		// borderColor: "#56E9CE",
+		// borderRadius: 50,
 	},
 	title: {
 		color: "#000",
@@ -177,12 +217,12 @@ const styles = StyleSheet.create({
 		color: "white",
 		fontSize: 14,
 		fontWeight: "bold",
-		marginBottom: 6
+		marginBottom: 6,
 	},
-	descText:{
+	descText: {
 		color: "white",
 		fontSize: 14,
-		marginBottom: 4
+		marginBottom: 4,
 	},
 	button: {
 		shadowColor: "#000",
@@ -206,12 +246,11 @@ const styles = StyleSheet.create({
 		resizeMode: "contain",
 		width: 50,
 		height: 50,
-		alignSelf: 'flex-start',
-		marginBottom: '2%',
-		marginLeft: '4%',
-		marginTop: '11%'
+		alignSelf: "flex-start",
+		marginBottom: "2%",
+		marginLeft: "4%",
+		marginTop: "11%",
 	},
-	
 });
 
 export default YouthServicesScreen;
