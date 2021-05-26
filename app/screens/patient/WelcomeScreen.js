ecea1e1122163ec3d4d2638054ef77ab85731275
Minhduc Cao <minhduc.t.cao@gmail.com>
2021-05-25 20:38:59 -0700

    Fixed skipping to Home screen
    Co-authored-by: kmphan <kmphan@users.noreply.github.com>

--- app/screens/patient/WelcomeScreen.js
+++ app/screens/patient/WelcomeScreen.js
@@ -20,29 +20,29 @@ import {
 } from "react-native-responsive-screen";
 import { processFontFamily } from "expo-font";
 import { LinearGradient } from "expo-linear-gradient";
-import {storeData, readData} from "../../utils/DataHandler.js"
+import { storeData, readData } from "../../utils/DataHandler.js";
 
 function WelcomeScreen({ navigation, props }) {
 	const [isLoading, setLoading] = useState(true);
-	// const [data, setData] = useState([]);
-	
-	useEffect(() => {
-		readData("log").then(
-			function (value){
-			var loggedIn = value;
-			// console.log(loggedIn);
-			
-			if(loggedIn == "True")
-			{
-				navigation.navigate("Home");
-			}
 
-			},
-			function (err) {
-				console.log(err);
+	useEffect(() => {
+		readData("log")
+			.then(
+				function (value) {
+					var loggedIn = value;
+
+					if (loggedIn == "User") {
+						navigation.navigate("Home");
+					} else if (loggedIn == "Admin") {
+						navigation.navigate("Admin Home");
+					}
+				},
+				function (err) {
+					console.log(err);
 				}
-			).finally(() => setLoading(false));
-		  }, []);
+			)
+			.finally(() => setLoading(false));
+	}, []);
 
 	return (
 		<View style={styles.container}>
@@ -53,41 +53,39 @@ function WelcomeScreen({ navigation, props }) {
 				end={{ x: 1, y: 1 }}
 				style={{ position: "absolute", top: 0, bottom: 0, left: 0, right: 0 }}
 			>
-						
-					<Image source={logo} style={styles.image} />
-					<Text></Text>
-					<Text
-						style={{
-							color: "#fff",
-							top: 165,
-							left: 34,
-							fontSize: hp("3.5%"),
-							textAlign: "left",
-						}}
-					>
-						Welcome to{"\n"}ConnecTeen
-					</Text>
-					<Text
-						style={{
-							color: "#fff",
-							top: 180,
-							left: 34,
-							fontSize: hp("2%"),
-							textAlign: "left",
-							width: 250,
-						}}
-					>
-						Get help finding resources, tracking your mood, and more.{" "}
-					</Text>
-					<TouchableOpacity
-						style={styles.button}
-						color="#f2f"
-						title="Get Started"
-						onPress={() => navigation.navigate("Login")}
-					>
-						<Text style={styles.buttonText}>Get Started</Text>
-					</TouchableOpacity>
-			
+				<Image source={logo} style={styles.image} />
+				<Text></Text>
+				<Text
+					style={{
+						color: "#fff",
+						top: 165,
+						left: 34,
+						fontSize: hp("3.5%"),
+						textAlign: "left",
+					}}
+				>
+					Welcome to{"\n"}ConnecTeen
+				</Text>
+				<Text
+					style={{
+						color: "#fff",
+						top: 180,
+						left: 34,
+						fontSize: hp("2%"),
+						textAlign: "left",
+						width: 250,
+					}}
+				>
+					Get help finding resources, tracking your mood, and more.{" "}
+				</Text>
+				<TouchableOpacity
+					style={styles.button}
+					color="#f2f"
+					title="Get Started"
+					onPress={() => navigation.navigate("Login")}
+				>
+					<Text style={styles.buttonText}>Get Started</Text>
+				</TouchableOpacity>
 			</LinearGradient>
 		</View>
 	);
