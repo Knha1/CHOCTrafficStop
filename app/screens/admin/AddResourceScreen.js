bcaa48fdafa0d30268d9c0637c0dcf2ba9f90722
Minhduc Cao <minhduc.t.cao@gmail.com>
2021-05-30 13:49:10 -0700

    Fixed error with adding resources

--- app/screens/admin/AddResourceScreen.js
+++ app/screens/admin/AddResourceScreen.js
@@ -40,186 +40,6 @@ function AddResourceScreen({ navigation }) {
 	const [email, setEmail] = useState("");
 
 	return (
-		// TODO: Potentially change this to ScrollView if fields get too long
-		// 		<SafeAreaView style={styles.container}>
-		// 			<Image
-		// 				style={styles.backArrow}
-		// 				source={backArrowWhite}
-		// 				onPress={() => navigation.navigate("Admin Home")}
-		// 			></Image>
-
-		// 			<View
-		// 				style={styles.bottomContainer}
-		// 				contentContainerStyle={styles.contentContainer}
-		// 			>
-		// 				<KeyboardAvoidingView
-		// 					behavior="height"
-		// 					style={{ flex: 1 }}
-		// 					enabled={true}
-		// 				>
-		// 					<Text style={styles.text}>Add Resource</Text>
-
-		// 					<Text style={styles.text3}>Title</Text>
-		// 					<TextInput
-		// 						style={styles.input}
-		// 						placeholder="Enter resource title"
-		// 						onChangeText={setTitle}
-		// 					/>
-
-		// 					<Text style={styles.text3}>Availability</Text>
-		// 					<TextInput
-		// 						style={styles.input}
-		// 						placeholder="Enter open hours"
-		// 						onChangeText={setAvailability}
-		// 					/>
-
-		// 					<Text style={styles.text3}>Phone Number</Text>
-		// 					<TextInput
-		// 						style={styles.input}
-		// 						placeholder="Enter phone number (if applicable)"
-		// 						onChangeText={setPhone}
-		// 					/>
-
-		// 					<Text style={styles.text3}>Email</Text>
-		// 					<TextInput
-		// 						style={styles.input}
-		// 						placeholder="Enter email (if applicable)"
-		// 						onChangeText={setEmail}
-		// 					/>
-
-		// 					<Text style={styles.text3}>Address</Text>
-		// 					<TextInput
-		// 						style={styles.input}
-		// 						placeholder="Enter street address (if applicable)"
-		// 						onChangeText={setAddress}
-		// 					/>
-
-		// 					<Text style={styles.text3}>Website</Text>
-		// 					<TextInput
-		// 						style={styles.input}
-		// 						placeholder="Enter website URL (if applicable)"
-		// 						onChangeText={setWebsite}
-		// 					/>
-
-		// 					<Text style={styles.text3}>Category</Text>
-		// 					<TextInput
-		// 						style={styles.input}
-		// 						placeholder="Enter resource category"
-		// 						onChangeText={setCategory}
-		// 					/>
-
-		// 					<Text style={styles.text3}>Description</Text>
-		// 					<TextInput
-		// 						style={styles.input}
-		// 						placeholder="Enter resource description"
-		// 						onChangeText={setDescription}
-		// 					/>
-
-		// 					<Text style={styles.text3}>Organization</Text>
-		// 					<Picker
-		// 						selectedValue={selectedOrganization}
-		// 						style={styles.dropdown}
-		// 						onValueChange={(itemValue, itemIndex) =>
-		// 							setSelectedOrganization(itemValue)
-		// 						}
-		// 					>
-		// 						<Picker.Item label="Project Choice" value="Project Choice" />
-		// 						<Picker.Item label="CHOC" value="CHOC" />
-		// 						<Picker.Item label="Waymakers" value="Waymakers" />
-		// 						<Picker.Item label="211OC" value="211OC" />
-		// 					</Picker>
-		// 				</KeyboardAvoidingView>
-
-		// 				<View style={{ position: "absolute", bottom: "15%" }}>
-		// 					<TouchableOpacity
-		// 						onPress={() => navigation.goBack()}
-		// 						style={styles.cancelButton}
-		// 					>
-		// 						<Text style={{ color: "#0E4B9D" }}>Cancel</Text>
-		// 					</TouchableOpacity>
-		// 					<TouchableOpacity
-		// 						onPress={() => {
-		// 							// TODO: Add checking to make sure specific fields have data
-		// 							var tagString = selectedOrganization
-		// 								.toLowerCase()
-		// 								.replace(" ", "-"); // Format to a tag
-		// 							var tag = { 0: tagString };
-		// 							var new_resource_id = null;
-		// 							firebase
-		// 								.database()
-		// 								.ref()
-		// 								.child("resource")
-		// 								.child("num_resources")
-		// 								.get()
-		// 								.then((snapshot) => {
-		// 									if (snapshot.exists()) {
-		// 										// Get the current resource_id and increment by 1 for new resource
-		// 										new_resource_id = snapshot.val() + 1;
-		// 									} else {
-		// 										console.log(
-		// 											"No 'num_resources' variable under 'resource' exists in the database"
-		// 										);
-		// 									}
-		// 								});
-
-		// 							firebase
-		// 								.database()
-		// 								.ref()
-		// 								.child("resource")
-		// 								.get()
-		// 								.then((snapshot) => {
-		// 									if (snapshot.exists()) {
-		// 										// Make sure resource_id is generated properly
-		// 										if (new_resource_id != null) {
-		// 											// Create resource object
-		// 											const new_resource_data = {
-		// 												address: address,
-		// 												availability: availability,
-		// 												category: category,
-		// 												description: description,
-		// 												email: email,
-		// 												name: title,
-		// 												organization: selectedOrganization,
-		// 												phone_num: phone,
-		// 												resource_id: new_resource_id,
-		// 												tags: tag,
-		// 												website: website,
-		// 											};
-
-		// 											firebase
-		// 												.database()
-		// 												.ref()
-		// 												.child("resource/" + new_resource_id)
-		// 												.set(new_resource_data)
-		// 												.then(() => {
-		// 													// Update num of resources (same as newly created resource_id)
-		// 													firebase
-		// 														.database()
-		// 														.ref()
-		// 														.child("resource/num_resources")
-		// 														.set(new_resource_id);
-		// 												});
-		// 										}
-		// 									} else {
-		// 										console.log("'resource' not found in database.");
-		// 									}
-		// 								});
-
-		// 							// TODO: Display confirmation that resource has been added
-		// 							// TODO: Redownload data from Firebase to refresh AsyncStorage
-
-		// 							navigation.navigate("Admin Home");
-		// 						}}
-		// 						style={styles.saveButton}
-		// 					>
-		// 						<Text style={{ color: "white" }}>Add Resource</Text>
-		// 					</TouchableOpacity>
-		// 				</View>
-		// 			</View>
-		// 		</SafeAreaView>
-		// 	);
-		// }
-
 		<View>
 			<ImageBackground
 				source={bg}
@@ -333,56 +153,80 @@ function AddResourceScreen({ navigation }) {
 									</TouchableOpacity>
 									<TouchableOpacity
 										onPress={() => {
+											// TODO: Add checking to make sure specific fields have data
 											var tagString = selectedOrganization
 												.toLowerCase()
 												.replace(" ", "-"); // Format to a tag
 											var tag = { 0: tagString };
-
+											var new_resource_id = null;
 											firebase
 												.database()
 												.ref()
-												.child("resource/" + data["resource_id"])
+												.child("resource")
+												.child("num_resources")
 												.get()
 												.then((snapshot) => {
-													// Make sure resource exists in DB before replacing
 													if (snapshot.exists()) {
-														// Create resource object
-														const edited_resource_data = {
-															address: address,
-															availability: availability,
-															category: category,
-															description: description,
-															email: email,
-															name: title,
-															organization: selectedOrganization,
-															phone_num: phone,
-															resource_id: data["resource_id"],
-															tags: tag,
-															website: website,
-														};
-
-														firebase
-															.database()
-															.ref()
-															.child("resource/" + data["resource_id"])
-															.set(edited_resource_data);
+														// Get the current resource_id and increment by 1 for new resource
+														new_resource_id = snapshot.val() + 1;
 													} else {
 														console.log(
-															"Resource with resource_id " +
-																data["resource_id"] +
-																" was not found in the database."
+															"No 'num_resources' variable under 'resource' exists in the database"
 														);
 													}
 												});
 
-											// TODO: Display confirmation that resource was successfully edited
+											firebase
+												.database()
+												.ref()
+												.child("resource")
+												.get()
+												.then((snapshot) => {
+													if (snapshot.exists()) {
+														// Make sure resource_id is generated properly
+														if (new_resource_id != null) {
+															// Create resource object
+															const new_resource_data = {
+																address: address,
+																availability: availability,
+																category: category,
+																description: description,
+																email: email,
+																name: title,
+																organization: selectedOrganization,
+																phone_num: phone,
+																resource_id: new_resource_id,
+																tags: tag,
+																website: website,
+															};
+
+															firebase
+																.database()
+																.ref()
+																.child("resource/" + new_resource_id)
+																.set(new_resource_data)
+																.then(() => {
+																	// Update num of resources (same as newly created resource_id)
+																	firebase
+																		.database()
+																		.ref()
+																		.child("resource/num_resources")
+																		.set(new_resource_id);
+																});
+														}
+													} else {
+														console.log("'resource' not found in database.");
+													}
+												});
+
+											// TODO: Display confirmation that resource has been added
+											// TODO: Redownload data from Firebase to refresh AsyncStorage
+
 											navigation.navigate("Admin Home");
 										}}
 										style={styles.saveButton}
 									>
-										<Text style={{ color: "white", textAlign: "center" }}>
-											Add Resource
-										</Text>
+										<Text style={{ color: "white" }}>Add Resource</Text>
 									</TouchableOpacity>
 								</View>
 							</View>
@@ -451,7 +295,7 @@ const styles = StyleSheet.create({
 		// left: 40,
 		// top: 36,
 		fontSize: 18,
-		marginBottom: "4%"
+		marginBottom: "4%",
 	},
 	cancelButton: {
 		//  position: "absolute",
