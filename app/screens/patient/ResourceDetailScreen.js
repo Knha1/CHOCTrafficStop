c1ebb487999c956ad2f31822b26e20e45a97014d
angiemrtin <angelaim@uci.edu>
2021-05-29 17:37:44 -0700

    added gradient in resource details pg

--- app/screens/patient/ResourceDetailScreen.js
+++ app/screens/patient/ResourceDetailScreen.js
@@ -2,21 +2,14 @@ import React, { useEffect, useState } from "react";
 import {
 	ImageBackground,
 	Linking,
-	SimpleSurvey,
 	StyleSheet,
 	Text,
-	TextInput,
 	View,
-	Button,
 	TouchableOpacity,
-	Icon,
-	TouchableHighlight,
 	Image,
 	ActivityIndicator,
 } from "react-native";
 import backArrowWhite from "../../assets/backArrowWhite.png";
-
-import colors from "../../config/colors";
 import bg from "../../assets/background.png";
 import { readData } from "../../utils/DataHandler";
 
@@ -50,13 +43,27 @@ function ResourceDetailScreen({ route, navigation }) {
 	}, [isLoading]);
 
 	return (
-		<View style={styles.container}>
-			<View>
-			<TouchableOpacity onPress={() => navigation.goBack()} style={styles.backArrow}>
-				<Image source={backArrowWhite} style={styles.backArrow}></Image>
-			</TouchableOpacity>
-			</View>
-			{isLoading ? (
+		<View style={[styles.container]}>
+			<ImageBackground
+				source={bg}
+				style={{
+					overflow: "hidden",
+					resizeMode: "stretch",
+					height: "100%",
+				}}
+			>
+				<TouchableOpacity onPress={() => navigation.goBack()}>
+					<Image source={backArrowWhite} style={styles.backButton}></Image>
+				</TouchableOpacity>
+				<View
+					style={[
+						styles.base,
+						{ height: "70%", padding: 20, alignItems: "flex-start" },
+					]}
+				>
+
+
+{isLoading ? (
 				// If still loading
 				<ActivityIndicator size="small" color="#0000ff" />
 			) : (
@@ -85,7 +92,7 @@ function ResourceDetailScreen({ route, navigation }) {
 								textDecorationLine: 'underline',
 								top: 20,
 								marginTop: 5,
-								left: 40,
+								//left: 35,
 								fontSize: 16,
 								width: 300,}}
 								onPress={() => {
@@ -100,94 +107,62 @@ function ResourceDetailScreen({ route, navigation }) {
 					</View>
 				 
 			)}
+				</View>
+			</ImageBackground>
 		</View>
 	);
 }
 
-var styles = StyleSheet.create({
+const styles = StyleSheet.create({
 	container: {
 		flex: 1,
-		//flexDirection: 'row',
-		backgroundColor: "#0066BB",
-		alignItems: "center",
-		justifyContent: "center",
-		paddingTop: 130,
 	},
-	backArrow: {
-		width: 34,
-		height: 34,
-		alignSelf: "flex-start",
-		justifyContent: 'center',
-		marginBottom: "2%",
-		marginTop: "-12%",
-		marginLeft: "-40%",
-	},
-	skipToResultsText: {
-		color: "#CAEDFF",
-		fontSize: 14,
-		position: "absolute",
-		top: 96,
-		paddingBottom: 100,
-	},
-	rectangle: {
-		height: 4,
-		width: 320,
-		borderRadius: 10,
-		position: "absolute",
-		top: 80,
-		backgroundColor: "#FFF",
+	buttonSpacing: {
+		margin: 5,
 	},
 	text: {
 		color: "#003C98",
 		top: 0,
-		left: 40,
+		//left: 35,
 		width: 300,
 		fontWeight: "bold",
 		fontSize: 24,
 	},
-	button: {
-		top: 55,
-		height: 45,
-		margin: 3,
-		width: 280,
-		borderRadius: 64,
-		alignSelf: "center",
-		backgroundColor: "#F8F8F8",
-		alignItems: "center",
-		justifyContent: "center",
-		borderColor: "#D6D6D6",
-		borderWidth: 1,
-	},
-	bottomContainer: {
-		flex: 1,
-		height: '100%',
-		alignSelf:"stretch",
-		backgroundColor: "white",
+	base: {
+		backgroundColor: "#F1F2F2",
 		borderTopRightRadius: 30,
 		borderTopLeftRadius: 30,
-		paddingTop: 35
-		},
-	buttonText: {
-		color: "#000",
-		alignSelf: "center",
+		alignSelf: "stretch",
+		flex: 1,
+		paddingTop: '10%',
+		paddingLeft: '10%'
+	},
+	backButton: {
+		resizeMode: "contain",
+		width: 34,
+		height: 34,
+		alignSelf: "flex-start",
+		marginBottom: "2%",
+		marginLeft: "4%",
+		marginTop: "11%",
 	},
 	infoText: {
 		color: "black",
 		top: 20,
 		marginTop: 5,
-		left: 40,
+		//left: 35,
 		fontSize: 16,
 		width: 300,
 	},
 	categoryText: {
 		fontSize: 16,
-		color: "black",
-		left: 40,
+		color: "black",	
+		//left: 35,
 	},
 	descriptionText: {
 		fontSize: 16,
 		width: 300,
-		left: 40,
+		//left: 35,
 		top: 60,
 	},
 });
