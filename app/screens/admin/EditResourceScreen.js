736428230a1a1a85279e69adb537cc0133dd177c
Minhduc Cao <minhduc.t.cao@gmail.com>
2021-05-30 16:48:46 -0700

    Cleaned code: Edit Resource
    Replaced Picker w/ non-deprecated version

--- app/screens/admin/EditResourceScreen.js
+++ app/screens/admin/EditResourceScreen.js
@@ -3,31 +3,28 @@ import {
 	Text,
 	View,
 	StyleSheet,
-	Button,
-	Modal,
-	Linking,
 	Image,
 	TouchableOpacity,
-	SafeAreaView,
 	TextInput,
 	KeyboardAvoidingView,
 	ScrollView,
-	ActivityIndicator,
-	Picker,
 	TouchableWithoutFeedback,
 	Keyboard,
 	ImageBackground,
 } from "react-native";
-// import DropDownPicker from "react-native-dropdown-picker";
-// TODO: Remove Picker import, replace with @react-native-community/picker
-
-import colors from "../../config/colors";
-import back from "../../assets/backArrowWhite.png";
-import edit from "../../assets/close.png";
-import { storeData, readData } from "../../utils/DataHandler.js";
+import { Picker } from "@react-native-picker/picker";
+import { readData } from "../../utils/DataHandler.js";
 import { firebase } from "../../firebase/config";
+// ASSET IMPORTS
+import back from "../../assets/backArrowWhite.png";
 import bg from "../../assets/background.png";
 
+/**
+ * Allows admin to edit resource details and push changes to DB
+ * @param {object} route - Contains resource_id to load resource details from
+ * @param {object} navigation - @react-navigation prop
+ * @returns - screen components
+ */
 function EditResourceScreen({ route, navigation }) {
 	var resource = route.params;
 	const resource_id = resource["resource_id"];
@@ -46,6 +43,7 @@ function EditResourceScreen({ route, navigation }) {
 	const [email, setEmail] = useState("");
 
 	useEffect(() => {
+		// Load in resource details from local storage
 		readData("resources")
 			.then((resources) => {
 				var resources = JSON.parse(resources);
@@ -75,156 +73,8 @@ function EditResourceScreen({ route, navigation }) {
 			.catch((error) => console.error(error))
 			.finally(() => setLoading(false));
 	}, [isLoading]);
+
 	return (
-		// TODO: Potentially change this to ScrollView if fields get too long
-		// <SafeAreaView style={styles.container}>
-		// 	<Image style={styles.backArrow} source={backArrowWhite}></Image>
-		// 	<View
-		// 		style={styles.bottomContainer}
-		// 		contentContainerStyle={styles.contentContainer}
-		// 	>
-		// 		<KeyboardAvoidingView
-		// 			behavior="height"
-		// 			style={{ flex: 1 }}
-		// 			enabled={true}
-		// 		>
-		// 			<Text style={styles.text}>Edit Resource</Text>
-
-		// 			<Text style={styles.text3}>Title</Text>
-		// 			<TextInput
-		// 				style={styles.input}
-		// 				onChangeText={setTitle}
-		// 				defaultValue={data["name"]}
-		// 			/>
-
-		// 			<Text style={styles.text3}>Availability</Text>
-		// 			<TextInput
-		// 				style={styles.input}
-		// 				onChangeText={setAvailability}
-		// 				defaultValue={data["availability"]}
-		// 			/>
-
-		// 			<Text style={styles.text3}>Phone Number</Text>
-		// 			<TextInput
-		// 				style={styles.input}
-		// 				onChangeText={setPhone}
-		// 				defaultValue={data["phone_num"]}
-		// 			/>
-
-		// 			<Text style={styles.text3}>Email</Text>
-		// 			<TextInput
-		// 				style={styles.input}
-		// 				onChangeText={setEmail}
-		// 				defaultValue={data["email"]}
-		// 			/>
-
-		// 			<Text style={styles.text3}>Address</Text>
-		// 			<TextInput
-		// 				style={styles.input}
-		// 				onChangeText={setAddress}
-		// 				defaultValue={data["address"]}
-		// 			/>
-
-		// 			<Text style={styles.text3}>Website</Text>
-		// 			<TextInput
-		// 				style={styles.input}
-		// 				onChangeText={setWebsite}
-		// 				defaultValue={data["website"]}
-		// 			/>
-
-		// 			<Text style={styles.text3}>Category</Text>
-		// 			<TextInput
-		// 				style={styles.input}
-		// 				onChangeText={setCategory}
-		// 				defaultValue={data["category"]}
-		// 			/>
-
-		// 			<Text style={styles.text3}>Description</Text>
-		// 			<TextInput
-		// 				style={styles.input}
-		// 				onChangeText={setDescription}
-		// 				defaultValue={data["description"]}
-		// 			/>
-
-		// 			<Text style={styles.text3}>Organization</Text>
-		// 			<Picker
-		// 				selectedValue={selectedOrganization}
-		// 				style={styles.dropdown}
-		// 				onValueChange={(itemValue, itemIndex) =>
-		// 					setSelectedOrganization(itemValue)
-		// 				}
-		// 			>
-		// 				<Picker.Item label="Project Choice" value="Project Choice" />
-		// 				<Picker.Item label="CHOC" value="CHOC" />
-		// 				<Picker.Item label="Waymakers" value="Waymakers" />
-		// 				<Picker.Item label="211OC" value="211OC" />
-		// 			</Picker>
-
-		// 			<TouchableOpacity
-		// 				onPress={() => navigation.goBack()}
-		// 				style={styles.cancelButton}
-		// 			>
-		// 				<Text style={{ color: "#0E4B9D" }}>Cancel</Text>
-		// 			</TouchableOpacity>
-		// 			<TouchableOpacity
-		// 				onPress={() => {
-		// 					var tagString = selectedOrganization
-		// 						.toLowerCase()
-		// 						.replace(" ", "-"); // Format to a tag
-		// 					var tag = { 0: tagString };
-
-		// 					firebase
-		// 						.database()
-		// 						.ref()
-		// 						.child("resource/" + data["resource_id"])
-		// 						.get()
-		// 						.then((snapshot) => {
-		// 							// Make sure resource exists in DB before replacing
-		// 							if (snapshot.exists()) {
-		// 								// Create resource object
-		// 								const edited_resource_data = {
-		// 									address: address,
-		// 									availability: availability,
-		// 									category: category,
-		// 									description: description,
-		// 									email: email,
-		// 									name: title,
-		// 									organization: selectedOrganization,
-		// 									phone_num: phone,
-		// 									resource_id: data["resource_id"],
-		// 									tags: tag,
-		// 									website: website,
-		// 								};
-
-		// 								firebase
-		// 									.database()
-		// 									.ref()
-		// 									.child("resource/" + data["resource_id"])
-		// 									.set(edited_resource_data);
-		// 							} else {
-		// 								console.log(
-		// 									"Resource with resource_id " +
-		// 										data["resource_id"] +
-		// 										" was not found in the database."
-		// 								);
-		// 							}
-		// 						});
-
-		// 					// TODO: Display confirmation that resource was successfully edited
-		// 					navigation.navigate("Admin Home");
-		// 				}}
-		// 				style={styles.saveButton}
-		// 			>
-		// 				<Text style={{ color: "white" }}>Save Changes</Text>
-		// 			</TouchableOpacity>
-		// 		</KeyboardAvoidingView>
-
-		// 		{/* <View style={{ position: "absolute", bottom: "15%" }}> */}
-
-		// 		{/* </View> */}
-		// 	</View>
-		// </SafeAreaView>
-
 		<View>
 			<ImageBackground
 				source={bg}
@@ -244,11 +94,8 @@ function EditResourceScreen({ route, navigation }) {
 					style={styles.container}
 				>
 					<ScrollView>
-						
 						<TouchableWithoutFeedback onPress={Keyboard.dismiss}>
 							<View style={styles.inner}>
-								{/* <Text style={styles.header}>Header</Text>
-          <TextInput placeholder="Username" style={styles.textInput} /> */}
 								<Text style={styles.text}>Edit Resource</Text>
 
 								<Text style={styles.text3}>Title</Text>
@@ -318,7 +165,7 @@ function EditResourceScreen({ route, navigation }) {
 								<Text style={styles.text3}>Organization</Text>
 								<Picker
 									selectedValue={selectedOrganization}
-									style={{ marginBottom: 28 }}
+									style={{ marginBottom: 40, width: "90%", marginLeft: "4%" }}
 									itemStyle={{ fontSize: 20 }}
 									onValueChange={(itemValue, itemIndex) =>
 										setSelectedOrganization(itemValue)
@@ -348,11 +195,13 @@ function EditResourceScreen({ route, navigation }) {
 									</TouchableOpacity>
 									<TouchableOpacity
 										onPress={() => {
+											// Temporarily assigning tag as organization
 											var tagString = selectedOrganization
 												.toLowerCase()
 												.replace(" ", "-"); // Format to a tag
 											var tag = { 0: tagString };
 
+											// Replace resource with edited resource details on DB
 											firebase
 												.database()
 												.ref()
@@ -389,8 +238,6 @@ function EditResourceScreen({ route, navigation }) {
 														);
 													}
 												});
-
-											// TODO: Display confirmation that resource was successfully edited
 											navigation.navigate("Admin Home");
 										}}
 										style={styles.saveButton}
@@ -516,96 +363,4 @@ const styles = StyleSheet.create({
 		marginTop: "14%",
 	},
 });
-// 	container: {
-// 		flex: 1,
-// 		backgroundColor: "#0066BB",
-// 		alignItems: "center",
-// 		justifyContent: "flex-start",
-// 		paddingTop: 100,
-// 	},
-// 	contentContainer: {
-// 		justifyContent: "flex-end",
-// 		//height: '90%',
-// 	},
-// 	input: {
-// 		height: 30,
-// 		width: "80%",
-// 		left: 40,
-// 		top: 36,
-// 		fontSize: 18,
-// 	},
-// 	backArrow: {
-// 		height: 34,
-// 		width: 34,
-// 		alignSelf: "flex-start",
-// 		left: 30,
-// 		bottom: 25,
-// 	},
-// 	text: {
-// 		color: "#003C98",
-// 		top: 34,
-// 		left: 40,
-// 		fontWeight: "bold",
-// 		fontSize: 24,
-// 	},
-// 	bottomContainer: {
-// 		flex: 1,
-// 		height: "100%",
-// 		alignSelf: "stretch",
-// 		backgroundColor: "white",
-// 		borderTopRightRadius: 30,
-// 		borderTopLeftRadius: 30,
-// 	},
-// 	buttonText: {
-// 		color: "#000",
-// 		alignSelf: "center",
-// 	},
-// 	text2: {
-// 		color: "black",
-// 		top: 50,
-// 		left: 40,
-// 		fontSize: 14,
-// 		width: 300,
-// 		marginTop: 5,
-// 	},
-// 	text3: {
-// 		fontSize: 16,
-// 		color: "black",
-// 		top: 38,
-// 		left: 40,
-// 		fontWeight: "bold",
-// 	},
-// 	cancelButton: {
-// 		position: "absolute",
-// 		margin: 3,
-// 		//top: '95%',
-// 		// bottom: "10%",
-// 		left: 40,
-// 		alignItems: "center",
-// 		justifyContent: "center",
-// 		backgroundColor: "white",
-// 		borderColor: "#0E4B9D",
-// 		borderRadius: 30,
-// 		borderWidth: 2,
-// 		height: 45,
-// 		width: 129,
-// 	},
-// 	saveButton: {
-// 		position: "absolute",
-// 		margin: 3,
-// 		left: 180,
-// 		//top: '95%',
-// 		alignItems: "center",
-// 		justifyContent: "center",
-// 		backgroundColor: "#0E4B9D",
-// 		borderRadius: 30,
-// 		height: 45,
-// 		width: 168,
-// 	},
-
-// 	dropdown: {
-// 		top: 30,
-// 	},
-// });
-
 export default EditResourceScreen;
